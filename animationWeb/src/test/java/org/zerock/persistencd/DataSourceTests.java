6340363c31da443fd04f209b80a90efb76bf33f4
clyne <clyne190804@gmail.com>
2019-10-14 19:59:56 +0900

    2019-10-14-19:57

--- animationWeb/src/test/java/org/zerock/persistencd/DataSourceTests.java
+++ animationWeb/src/test/java/org/zerock/persistencd/DataSourceTests.java
@@ -23,48 +23,21 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class DataSourceTests {
 
-  @Setter(onMethod_ = { @Autowired })
-  private DataSource dataSource;
-  @Test
-  public void testConnection() {
-	  try(Connection con = dataSource.getConnection()){
-		  log.info(con);
-	  }catch(Exception e) {
-		  fail(e.getMessage());
-	  }
-  }
-//  @Setter(onMethod_ = { @Autowired })
-//  private SqlSessionFactory sqlSessionFactory;
-//
-//  @Test
-//  public void testMyBatis() {
-//
-//    try (SqlSession session = sqlSessionFactory.openSession();
-//       Connection con = session.getConnection();
-//      ) {
-//
-//      log.info(session);
-//      log.info(con);
-//
-//    } catch (Exception e) {
-//      fail(e.getMessage());
-//    }
-//
-//  }
-//
-//
-//  @Test
-//  public void testConnection() {
-//    
-//    
-//    try (Connection con = dataSource.getConnection()){
-//
-//      log.info(con);      
-//      
-//    }catch(Exception e) {
-//      fail(e.getMessage());
-//    }
-//  }
+	@Setter(onMethod_ = { @Autowired })
+	private DataSource dataSource;
+
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testConnection() {
+		try (SqlSession session = sqlSessionFactory.openSession();
+				Connection con = dataSource.getConnection()) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
 }
-
-
